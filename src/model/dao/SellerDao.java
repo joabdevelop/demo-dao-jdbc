e774e233036160982dc57469401ffb1f25dc2828
joabdevelop <joabdeveloper@gmail.com>
2026-06-27 17:34:22 -0300

    DepartmentDao and SellerDao interfaces

--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -6,15 +6,15 @@ import model.entities.Department;
 import model.entities.Seller;
 
 public interface SellerDao {
-    void insert(Seller obj);
+    void insert(Seller obj); // Insere um vendedor
 
-    void update(Seller obj);
+    void update(Seller obj); // Atualiza um vendedor
 
-    void deleteById(Integer id);
+    void deleteById(Integer id); // Deleta um vendedor por id
 
-    Seller findById(Integer id);
+    Seller findById(Integer id); // Busca um vendedor por id
 
-    List<Seller> findAll();
+    List<Seller> findAll(); // Busca todos os vendedores
 
-    List<Seller> findByDepartment(Department department);
+    List<Seller> findByDepartment(Department department); // Busca vendedores por departamento
 }
