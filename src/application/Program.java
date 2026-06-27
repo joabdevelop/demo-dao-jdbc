0b8a9aa4d21d292afcf03fe75544792e864f8983
joabdevelop <joabdeveloper@gmail.com>
2026-06-27 15:59:52 -0300

    feat: setup inicial do projeto java jdbc e conexao

---
+++ src/application/Program.java
@@ -0,0 +1,18 @@
+package application;
+
+import java.sql.Connection;
+import db.DB;
+
+public class Program {
+    public static void main(String[] args) {
+        System.out.println("=== Teste de Conexao ===");
+        try {
+            Connection conn = DB.getConnection();
+            System.out.println("Conectado com sucesso ao MySQL!");
+            DB.closeConnection();
+            System.out.println("Conexao fechada de forma segura.");
+        } catch (Exception e) {
+            System.out.println("Erro ao tentar conectar: " + e.getMessage());
+        }
+    }
+}
