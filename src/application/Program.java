aeb7d7a6b31424ff6fb9601d7cdf5ab9e6c93f19
joabdevelop <joabdeveloper@gmail.com>
2026-06-27 16:30:48 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,11 @@
 package application;
 
 import java.sql.Connection;
+import java.util.Date;
+
 import db.DB;
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
     public static void main(String[] args) {
@@ -18,5 +21,8 @@ public class Program {
 
         Department dep = new Department(1, "Software");
         System.out.println(dep);
+
+        Seller seller = new Seller(1, "Bob", "bob@gmail.com", new Date(), 3000.00, dep);
+        System.out.println(seller);
     }
 }
